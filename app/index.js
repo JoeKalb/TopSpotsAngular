4a3539613a1f3fc5581fd3a55cae0343ba9472af
Jospeh Kalb <kalb.joseph@gmail.com>
2016-09-30 13:38:13 -0700

    second commit

---
+++ app/index.js
@@ -0,0 +1,23 @@
+// top level application
+myApp = angular.module('myApp', []);
+
+// creating the controller functionality 
+myApp.controller('MainController', ['$scope', '$http',  function($scope, $http){ //getting the $scope and $http functions
+	$http.get('topspots.json') // acquiring the JSON file
+	.then(function (potato) { // ".then" using the info in the file
+		$scope.bakinsoda = potato.data.topspots; // putting "places" into "$scope" and accessing the topspots data
+	});
+
+	$scope.addData = function (newName, newDes, newLoc)
+    {
+    	var config = {
+        	params: {
+        		name: newName ,
+        		description: newDes ,
+        		location: newLoc
+        	}
+      	};
+      	$scope.stuff = config;
+		
+	};	
+}]);
