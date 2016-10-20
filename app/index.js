7f54f6163cceaba7c02088ec0e24f50afbeb65c2
JoeKalb <kalb.joseph@gmail.com>
2016-10-20 16:22:17 -0700

    new commit

--- app/index.js
+++ app/index.js
@@ -3,9 +3,9 @@ myApp = angular.module('myApp', []);
 
 // creating the controller functionality 
 myApp.controller('MainController', ['$scope', '$http',  function($scope, $http){ //getting the $scope and $http functions
-	$http.get('topspots.json') // acquiring the JSON file
+	$http.get('http://localhost:55519/api/TopSpots') // acquiring the JSON file
 	.then(function (potato) { // ".then" using the info in the file
-		$scope.bakinsoda = potato.data.topspots; // putting "places" into "$scope" and accessing the topspots data
+		$scope.bakinsoda = potato.data; // putting "places" into "$scope" and accessing the topspots data
 	});
 
 	$scope.addData = function (newName, newDes, newLoc)
